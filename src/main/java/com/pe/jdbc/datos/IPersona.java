c0e55333a9cc1dc0f96a42db946009f19643cb2b
AngelGCrz <angelgcrz@gmail.com>
2023-06-21 22:28:48 -0500

    implements interface Persona CRUD

--- src/main/java/com/pe/jdbc/datos/IPersona.java
+++ src/main/java/com/pe/jdbc/datos/IPersona.java
@@ -1,5 +1,20 @@
 package com.pe.jdbc.datos;
 
-public class IPersona {
+import java.util.List;
 
+import com.pe.jdbc.domain.PersonaDTO;
+
+public interface IPersona {
+
+	//interface para listar Personas
+	public List<PersonaDTO> listPersonas();
+	
+	//interface para insertar Personas
+	public int insertPersonas(PersonaDTO persona);
+	
+	//interface para actualizar personas
+	public int updatePersonas(PersonaDTO persona);
+	
+	//interface para eliminar personas
+	public int deletePersonas(PersonaDTO persona);
 }
