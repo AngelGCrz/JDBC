7bd9c50a28465e0f1dacf70e3b8fe81aee6c2d64
AngelGCrz <angelgcrz@gmail.com>
2023-06-21 22:07:35 -0500

    implement class conexion

--- src/main/java/com/pe/jdbc/datos/Conexion.java
+++ src/main/java/com/pe/jdbc/datos/Conexion.java
@@ -1,6 +1,32 @@
 package com.pe.jdbc.datos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Conexion {
 
+	private static final String JDBC_URL = "jdbc:mysql//localhost:3306/test?useSSL=false&useTimeZone=true&serverTimeZone=UTF&AllorPublicKeyRetrieval=true";
+	private static final String JDBC_ROOT = "root";
+	private static final String JDBC_PASS = "1224";
 	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(JDBC_URL, JDBC_ROOT, JDBC_PASS);
+	}
+	
+	public void close(PreparedStatement pst) throws SQLException {
+		pst.close();
+	}
+	public void close(Statement st) throws SQLException {
+		st.close();
+	}
+	public void close(ResultSet rs) throws SQLException {
+		rs.close();
+	}
+	public void close(Connection con) throws SQLException {
+		con.close();
+	}
 }
